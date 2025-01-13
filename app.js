9215a9cf22088cad021438dfa83136c869ee5a19
wnkinc <140204064+wnkinc@users.noreply.github.com>
2025-01-12 21:39:08 -0800

    Change port info in hopes of working on azure

--- app.js
+++ app.js
@@ -101,7 +101,71 @@ process.on("SIGTERM", async () => {
   process.exit(0); // Exit the process
 });
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Express app - listening on port ${PORT}!`);
-});
+// const PORT = process.env.PORT || 8080;
+// app.listen(PORT, "0.0.0.0", () => {
+//   console.log(`Express app - listening on port ${PORT}!`);
+// });
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+function onError(error) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  var bind = typeof PORT === "string" ? "Pipe " + PORT : "Port " + PORT;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  console.log("Listening on " + bind);
+}
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+var PORT = normalizePort(process.env.PORT || "8080");
+app.set("port", PORT);
+
+var server = require("http").createServer(app);
+server.listen(PORT);
+server.on("error", onError);
+server.on("listening", onListening);
